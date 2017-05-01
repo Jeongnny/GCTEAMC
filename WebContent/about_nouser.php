6c2df155ffeae4a7c3d432201dbca562bb6a2a44
Jeongeun Kim <kjeun421@gmail.com>
2017-05-01 03:46:10 +0100

    Insert about_body.html and about_nouser.php in WebContent

---
+++ WebContent/about_nouser.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hotel Booking Extravaganza</title>
+
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="homestyle.css" />
+
+</head>
+<body>
+	<div>
+		<nav class="navbar navbar-inverse">
+
+		<div class="container">
+			<!-- Navbar Header -->
+			<?php include 'navbar_nouser.php'; ?>
+		</div>
+	</nav>
+
+	<div class="container">
+		<?<?php include 'about_body.html'; ?>
+	</div>
+
+	</div>
+</body>
+</html>
